dd37cc69f3b4ceda7904e494372232d881c9acf5
Giriraj Thanvi <giriraj.thanvi@mindbowser.com>
2026-03-31 12:41:07 +0530

    fix:delete unwanted files

--- Day5/buggy.js
+++
@@ -1,9 +0,0 @@
-function calculateTotal(prices) {
-  let total = 0;
-  //error happens as its goes till at index=3 where its undefined and add NaN to total creating a blunder
-  for (let i = 0; i <= prices.length; i++) {
-    total += prices[i];
-  }
-  return total;
-}
-console.log(calculateTotal([10, 20, 30]));
