45ab3597a01bcec4a429488729a16fb23c203456
Giriraj Thanvi <giriraj.thanvi@mindbowser.com>
2026-04-14 08:47:20 +0530

    Day19Task

--- app.js
+++ app.js
@@ -3,20 +3,38 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
+const MONGODB_URI =
+  'mongodb+srv://giriraj1:ram@cluster0.hg0dbh5.mongodb.net/?appName=Cluster0';
+
 const app = express();
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: 'sessions'
+});
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  session({
+    secret: 'my secret',
+    resave: false,
+    saveUninitialized: false,
+    store: store
+  })
+);
 
 app.use((req, res, next) => {
   User.findById('5bab316ce0a7c75f783cb8a8')
@@ -29,19 +47,18 @@ app.use((req, res, next) => {
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404);
 
 mongoose
-  .connect(
-    'mongodb+srv://giriraj1:ram@cluster0.hg0dbh5.mongodb.net/?appName=Cluster0'
-  )
+  .connect(MONGODB_URI)
   .then(result => {
     User.findOne().then(user => {
       if (!user) {
         const user = new User({
-          name: 'GT',
-          email: 'gt@test.com',
+          name: 'Max',
+          email: 'max@test.com',
           cart: {
             items: []
           }
