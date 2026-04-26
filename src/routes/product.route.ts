fbc0c65c242531cbe5cb40f92505e8123550158e
Giriraj Thanvi <giriraj.thanvi@mindbowser.com>
2026-04-27 01:20:35 +0530

    Day25Task

---
+++ src/routes/product.route.ts
@@ -0,0 +1,22 @@
+import express from 'express'
+import { upload } from "../utils/multer.js";
+import { getProducts, postAddProduct } from '../controller/product.controller';
+
+
+
+
+const router = express.Router();
+
+
+router.get('/products',getProducts)
+router.post(
+  "/add-product",
+  upload.single("image"),
+  postAddProduct
+);
+
+
+
+
+export default router;
+
